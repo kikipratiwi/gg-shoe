07a78f088e1d3a45035bad330b8c34faafd370d8
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-27 01:09:22 +0700

    refactor(section-data): update title and imageUrl

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,29 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: 'oxford',
+          imageUrl: 'https://i.ibb.co/mtr3c7L/3807a0683ce645448646facde754043f.jpg',
           id: 1
         },
         {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          title: 'heels',
+          imageUrl: 'https://i.ibb.co/HPVbbZ2/f9358c4f2ad9fbc6118cbae5096aed0d.jpg',
           id: 2
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://i.ibb.co/smJByHs/9701a444f1c1fb3cf2b700811f1ffc7a.jpg',
           id: 3
         },
         {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          title: 'boots',
+          imageUrl: 'https://i.ibb.co/X2ycnsv/b02fe37cf55263df181304e214e19ee7.jpg',
           size: 'large',
           id: 4
         },
         {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          title: 'flats',
+          imageUrl: 'https://i.ibb.co/2S5RZth/560bba1dc6e129fd36b8393082dd20fd.jpg',
           size: 'large',
           id: 5
         }
