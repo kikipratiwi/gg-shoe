26a713a9e13888ae638a1f49e3126b2a995723ec
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-27 01:10:32 +0700

    feat(menu-item): load image as background

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div className='menu-item'>
+  <div 
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }} 
+    className='menu-item'
+  >
     <div className='content'>
       <h1 className='title'>{title}</h1>
       <span className='subtitle'>SHOP NOW</span>
