c0cb9fc1c60aaf29de387a4990e226ee8270da38
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-06-07 22:54:57 +0700

    refactor(router): update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+import './App.css';
+
+const OxfordsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>OXFORDS PAGE </h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/oxfords' component={OxfordsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,31 +11,36 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'oxford',
+          title: 'oxfords',
           imageUrl: 'https://i.ibb.co/mtr3c7L/3807a0683ce645448646facde754043f.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'oxfords'
         },
         {
           title: 'heels',
           imageUrl: 'https://i.ibb.co/HPVbbZ2/f9358c4f2ad9fbc6118cbae5096aed0d.jpg',
-          id: 2
+          id: 2,
+          linkUrl: 'heels'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/smJByHs/9701a444f1c1fb3cf2b700811f1ffc7a.jpg',
-          id: 3
+          id: 3,
+          linkUrl: 'sneakers'
         },
         {
           title: 'boots',
           imageUrl: 'https://i.ibb.co/X2ycnsv/b02fe37cf55263df181304e214e19ee7.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: 'boots'
         },
         {
           title: 'flats',
           imageUrl: 'https://i.ibb.co/2S5RZth/560bba1dc6e129fd36b8393082dd20fd.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: 'flats'
         }
       ]  
     } 
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return(
       <div className='directory-menu'>
-        {this.state.sections.map(({title, imageUrl, id, size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
