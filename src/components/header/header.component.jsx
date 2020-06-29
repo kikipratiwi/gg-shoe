dc78547b7c9e2f2e2acc26b7b49883bc73ba7d62
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-06-29 22:45:03 +0700

    feat(google-sign-in): integrate firebase google sign in into application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/shoe.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link to='/'>
       <Logo className='logo-container' />
@@ -17,6 +18,16 @@ const Header = () => (
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
+      {
+        currentUser?
+        <div className='option' onClick={ () => auth.signOut() }>
+          SIGN OUT
+        </div>
+        :
+        <Link className='option' to='/signIn'>
+          SIGN IN 
+        </Link>
+      }
     </div>
   </div>
 );
