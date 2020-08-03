b55ee3d754ec6d1d0f8ada193e8041519149eecd
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-08-03 14:13:05 +0700

    fix(sign-in-and-sign-up): fix failure sign-in and sign-up algorithm

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
     measurementId: "G-DMBKJZB0SX"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
@@ -22,13 +24,13 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     if (!snapShot.exists) {
         const { displayName, email } = userAuth;
-        const cteatedAt = new Date();
+        const createdAt = new Date();
 
         try {
             await userRef.set({
                 displayName,
                 email,
-                cteatedAt,
+                createdAt,
                 ...additionalData
             });
         } catch (error) {
@@ -38,9 +40,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     return userRef;
 };
-
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
