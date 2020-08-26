635c16560efaed42e263ac00490caa4fa921ccca
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-08-26 16:21:46 +0700

    refactor(shop): move collections overview into owns component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../componets/collections-overview/collections-overview.component';
 
 import './shop.styles.scss';
 
-const shopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionItems }) => (
-      <CollectionPreview key={id} {...otherCollectionItems} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
-
-export default connect(mapStateToProps)(shopPage);
+export default ShopPage;
